45b2e361acd6c726f01f43ad49a4eb91c5f2fd00
tgha <gkxorud111@gmail.com>
2020-08-07 23:52:37 +0900

    로그인기능

--- src/main/java/com/hayang/blog/application/login/web/LoginController.java
+++ src/main/java/com/hayang/blog/application/login/web/LoginController.java
@@ -5,6 +5,8 @@ import com.hayang.blog.application.login.service.impl.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -15,9 +17,11 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
 
+    @ResponseBody
     @PostMapping(value = "/member/login")
-    public Map<String, String> login(LoginParams loginParams) {
+    public Map<String, String> login(@RequestParam LoginParams loginParams) {
         Map<String, String> response = new HashMap<>();
+        System.out.println("컨트롤러?");
 
         try {
             loginService.login(loginParams);
