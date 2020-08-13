78cb501c350c9f288ff945bd0dc92b7d7f5a8b3e
tgha <gkxorud111@gmail.com>
2020-08-13 21:50:04 +0900

    static 경로 설정 중

--- src/main/java/com/hayang/blog/application/login/web/LoginController.java
+++ src/main/java/com/hayang/blog/application/login/web/LoginController.java
@@ -3,10 +3,13 @@ package com.hayang.blog.application.login.web;
 import com.hayang.blog.application.login.domain.LoginParams;
 import com.hayang.blog.application.login.service.impl.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -16,6 +19,12 @@ public class LoginController {
 
     @Autowired
     private LoginService loginService;
+
+    @GetMapping(value = "/member/login/view")
+    public ModelAndView getLoginView(ModelAndView mnv) {
+        mnv.setViewName("member/login");
+        return mnv;
+    }
 
     @ResponseBody
     @PostMapping(value = "/member/login")
